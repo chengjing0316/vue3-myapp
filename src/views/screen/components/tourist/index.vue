484c15760a09726da2a0877dcd63f5748dd6b5e0
chengjing-7DI25 <cj976900401@outlook.com>
2024-06-25 21:16:33 +0800

    feat:数据大屏1

---
+++ src/views/screen/components/tourist/index.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="box">
+    <div class="top">
+      <p class="title">实时游客统计</p>
+      <p class="bg"></p>
+      <p class="right">
+        可预约总量
+        <span>99999</span>
+        人
+      </p>
+    </div>
+    <div class="number">
+      <span v-for="(item, index) in people" :key="index">{{ item }}</span>
+    </div>
+    <!-- 展示图表 -->
+    <div class="charts"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import * as echarts from 'echarts'
+let people = ref('215908')
+let charts = ref()
+onMounted(() => {
+  //获取echarts类的示例
+  let mycharts = echarts.init(charts.value)
+  //设置实例的配置项
+  mycharts.setOption({
+    title: { text: 'water' },
+    xAxis: {},
+    yAxis: {},
+    series: { type: 'bar', data: [10, 20, 30, 40] },
+    grid: {
+      left: 0,
+      top: 0,
+      bottom: 0,
+      right: 0
+    }
+  })
+})
+</script>
+
+<style scoped lang="scss">
+.box {
+  background: url(../../images/dataScreen-main-lb.png) no-repeat;
+  background-size: 100% 100%;
+  margin-top: 10px;
+  .top {
+    margin-left: 20px;
+    .title {
+      color: white;
+      font-size: 25px;
+    }
+    .bg {
+      width: 68px;
+      height: 7px;
+      background: url(../../images/dataScreen-title.png);
+      background-size: 100% 100%;
+      margin-top: 10px;
+    }
+    .right {
+      float: right;
+      color: white;
+      font-size: 20px;
+      span {
+        color: yellowgreen;
+      }
+    }
+  }
+  .number {
+    padding: 10px;
+    margin-top: 30px;
+    display: flex;
+    span {
+      flex: 1;
+      height: 40px;
+      text-align: center;
+      line-height: 40px;
+      background: url(../../images/total.png);
+      background-size: 100% 100%;
+      color: #29fcfc;
+    }
+  }
+  .charts {
+    width: 100%;
+    height: 250px;
+    background-color: red;
+  }
+}
+</style>
--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,7 +1,94 @@
 <template>
-  <div>大屏</div>
+  <div class="container">
+    <!-- 展示内容区域 -->
+    <div class="screen" ref="screen">
+      <!-- 顶部 -->
+      <div class="top">
+        <Top></Top>
+      </div>
+      <div class="bottom">
+        <div class="left">
+          <Tourist class="tourist"></Tourist>
+          <Sex class="sex"></Sex>
+          <Age class="age"></Age>
+        </div>
+        <div class="center"></div>
+        <div class="right"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+//引入顶部的子组件
+import Top from './components/top/index.vue'
+//引入左侧三个子组件
+import Tourist from './components/tourist/index.vue'
+import Sex from './components/sex/index.vue'
+import Age from './components/age/index.vue'
+//获取展示内容盒子DOM
+let screen = ref()
 
-<style scoped lang="scss"></style>
+//计算缩放比例
+function getScale(w = 1920, h = 1080) {
+  const ww = window.innerWidth / w
+  const wh = window.innerHeight / h
+  return Math.min(ww, wh)
+}
+
+//监听视口变化
+window.onresize = () => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
+}
+onMounted(() => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
+})
+</script>
+
+<style scoped lang="scss">
+.container {
+  width: 100vw;
+  height: 100vh;
+  background: url(./images/bg.png) no-repeat;
+  background-size: cover;
+  .screen {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform-origin: left top;
+    width: 1920px;
+    height: 1080px;
+    .top {
+      width: 100%;
+      height: 40px;
+    }
+    .bottom {
+      display: flex;
+      .right {
+        flex: 1;
+      }
+      .left {
+        flex: 1;
+        height: 1040px;
+        display: flex;
+        flex-direction: column;
+        .tourist {
+          flex: 1.2;
+        }
+        .sex {
+          flex: 1;
+          background: orange;
+        }
+        .age {
+          flex: 1;
+          background-color: honeydew;
+        }
+      }
+      .center {
+        flex: 2;
+      }
+    }
+  }
+}
+</style>
